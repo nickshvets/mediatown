332851e04f0f7de86c64071570c6eabdc565a28c
Nick Shvets <nick.shvets.dev@gmail.com>
2023-11-17 21:47:32 +0200

    Implementing general components

---
+++ src/components/ButtonsContainer.tsx
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+const ButtonsContainer = styled.div``;
+
+export default ButtonsContainer;
---
+++ src/components/Container.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+import theme from '../theme';
+
+const Container = styled.div``;
+
+export default Container;
